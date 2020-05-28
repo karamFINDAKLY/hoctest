032ec0287714f9fedf65c898cc1b6b31e085e2ec
karamFINDAKLY <51822543+karamFINDAKLY@users.noreply.github.com>
2020-05-28 10:05:26 +0200

    first commit

---
+++ src/Entity/User.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ *@ORM\Table(name="users")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="simple_array")
+     */
+    private $pass = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPass(): ?array
+    {
+        return $this->pass;
+    }
+
+    public function setPass(array $pass): self
+    {
+        $this->pass = $pass;
+
+        return $this;
+    }
+}
